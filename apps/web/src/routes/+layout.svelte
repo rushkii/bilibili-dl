bd6a18b3b95fc64d8a27f8f60dd9a4d1d9d78925
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-08-29 01:05:19 +0700

    feat: add main `<svelte:head/>`
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -2,4 +2,13 @@
   import "../app.css";
 </script>
 
+
+<svelte:head>
+  <title>OpenBstation</title>
+  <meta
+    name="viewport"
+    content="minimum-scale=1, initial-scale=1, width=device-width"
+  />
+</svelte:head>
+
 <slot />
